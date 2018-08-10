6105a667544f21c878ec1534d210cfed98ca43dc
nathaliamoreno <41310257+nathaliamoreno@users.noreply.github.com>
2018-08-09 22:41:32 -0300

    Update 3 - main.cpp

--- 3 - main.cpp
+++ 3 - main.cpp
@@ -6,9 +6,13 @@ using namespace std;
 
 int main()
 {
- 	    Invoice fatura = Invoice(1, 2, "brinquedo", 20.50);
+ 	    Invoice fatura1 = Invoice(1, 2, "brinquedo", 20.50);
+ 	    Invoice fatura2 = Invoice (1, 2, "brinquedo", -5.0);
+ 	    Invoice fatura3 = Invoice(1, -2, "brinquedo", 20.50);
 
-        cout << fatura.getInvoiceAmount() <<endl;
+        cout << "Total da fatura com valores MAIORES que zero: R$ " << fatura1.getInvoiceAmount() <<endl;
+        cout << "Total da fatura com PRECO MENOR que zero: R$ " << fatura2.getInvoiceAmount() <<endl;
+        cout << "Total da fatura com QUANTIDADE MENOR que zero: R$ " << fatura3.getInvoiceAmount() <<endl;
 
 
     return 0;
