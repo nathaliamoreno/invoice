e5518f45b951aeb4ff832ae2341674c4b11ade91
nathaliamoreno <41310257+nathaliamoreno@users.noreply.github.com>
2018-08-15 00:15:22 -0300

    Update 3 - main.cpp

--- 3 - main.cpp
+++ 3 - main.cpp
@@ -6,14 +6,19 @@ using namespace std;
 
 int main()
 {
- 	    Invoice fatura1 = Invoice(1, 2, "brinquedo", 20.50);
- 	    Invoice fatura2 = Invoice (1, 2, "brinquedo", -5.0);
- 	    Invoice fatura3 = Invoice(1, -2, "brinquedo", 20.50);
+ 	    Invoice fatura1 = Invoice(1, 2, "brinquedo", 20.50); //VALORES POSITIVOS
+ 	    Invoice fatura2 = Invoice (1, 2, "brinquedo", -5.0); //PREÇO NEGATIVO
+ 	    Invoice fatura3 = Invoice(1, -2, "brinquedo", 20.50); //QUANTIDADE NEGATIVA
 
-        cout << "Total da fatura com valores MAIORES que zero: R$ " << fatura1.getInvoiceAmount() <<endl;
-        cout << "Total da fatura com PRECO MENOR que zero: R$ " << fatura2.getInvoiceAmount() <<endl;
-        cout << "Total da fatura com QUANTIDADE MENOR que zero: R$ " << fatura3.getInvoiceAmount() <<endl;
-
+ 	    cout << "---------------------------" <<endl;
+        cout << "Total da fatura:\n R$ " << fatura1.getInvoiceAmount() <<endl;
+        cout << "---------------------------" <<endl;
+        cout << "Total da fatura com PRECO < 0:\n R$ " << fatura2.getInvoiceAmount() <<endl;
+        cout << "(preco negativo configurado como zero)" <<endl;
+        cout << "---------------------------" <<endl;
+        cout << "Total da fatura com QUANTIDADE < 0:\n R$ " << fatura3.getInvoiceAmount() <<endl;
+        cout << "(quantidade negativa configurada como zero)" <<endl;
+        cout << "---------------------------" <<endl;
 
     return 0;
 }
